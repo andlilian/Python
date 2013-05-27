4d87ca472f333cdd9d30caa5f1c4486d1cd2ce54
andlilian <andlilian@gmail.com>
2013-05-26 19:33:29 -0600

    first commit

---
+++ Python/Funciones phyton/Listas.py
@@ -0,0 +1,39 @@
+# LISTAS
+
+# len: sirve para saber cuantos elementos hay en una lista
+t = [1,2,3]
+len(t)
+
+
+# insert: sirve para anadir un elemento insertandolo en una posicion concreta
+
+lis=[1,8,'tres','cinco']
+lis.insert(2,'emacs')
+lis
+
+
+
+# sumar listas
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+lista3 = lista1 + lista2
+
+# para ver el valor maximo o minimo que tiene una lista
+max(lista1)
+min(lista2)
+max(lista3)
+
+#sorted para ordenar los elementos de una lista
+lista4 = [1,12,6,20,10]
+lista5 = sorted(lista4)
+
+
+#CADENAS
+
+# split para separar una cadena
+cad= "Hoy sera un gran dia"
+cad.split()
+
+# replace para reemplazar una parte de una cadena por otra
+cadena = "Esto sera reemplazado: hola"
+print cadena.replace('hola', 'mundo')
